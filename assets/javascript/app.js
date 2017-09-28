0d3e2d94c9e1457f8878cf3ea893d6d97702d3d3
ykeanu <ykhernan@ucla.edu>
2017-09-28 15:38:27 -0700

    Fixed default functionality + added basic styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,45 @@
 $(document).ready(function() {
 
-    var topics = [];
+    // Automatically pre-set topics
+    var topics = ["Skydiving", "Programming", "Mountain Climbing", "Memes", "DJ Khalid"];
 
+    // Accesses Giffy API and places the image on the page
     function displayGifs() {
-        var topic = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=oN3X6jHiRb3vdFf66lYv6z1VmUGL2Hb6&limit=10";
+        var topicSearch = $(this).attr("data-name").replace(" ", "+");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicSearch + "&api_key=oN3X6jHiRb3vdFf66lYv6z1VmUGL2Hb6&limit=9";
         var test = $(this);
          $("#topic-gifs").empty();
 
-
+         // Giffy API call
         $.ajax({
             url: queryURL,
             method: "GET"
         }).done(function(response) {
             for (var i = 0; i < response.data.length; i++) {
-                var topicDiv = $("<div class='etc'>"); //Create container div
-
-                var animateURL = response.data[i].images.fixed_height.url; //Store image
+                var topicDiv = $("<div class='etc col-lg-4 col-md-4 col-sm-6 col-xs-12'>"); 
+                var animateURL = response.data[i].images.fixed_height.url; 
                 var stillURL = response.data[i].images.fixed_height_still.url;
-                var imgTag = $("<img>"); //Create image tag
-                imgTag.attr("src", animateURL); //Create src attritbute to display image
+                var imgTag = $("<img>"); 
+                imgTag.attr("src", animateURL); 
                 imgTag.data("state", "animate");
                 imgTag.data("animate", animateURL);
                 imgTag.data("still", stillURL);
                 imgTag.addClass("animation");
 
-                var rating = "Rating: " + response.data[i].rating;  //Store rating
-                var pTag = $("<p>"); // Create p tag
-                pTag.html(rating); // Insert rating
+                var rating = "Rating: " + response.data[i].rating;  
+                var pTag = $("<p>"); 
+                pTag.html(rating);
 
-                topicDiv.append(imgTag); //Append, adds to the end of elements
-                topicDiv.prepend(pTag); //Prepend, adds to the beginning of elements
+                topicDiv.append(imgTag);
+                topicDiv.prepend(pTag);
 
                 $("#topic-gifs").append(topicDiv);
-
-                // ** TEST **
-                console.log(topicDiv, [i]);
             };
 
-
-        // ** TEST **
-            console.log(response, "GIPHY API");
-            // console.log (response.data.length, "LENGTH");
-            // console.log(response.data[0].images.fixed_height.url);
         });
     }
 
+    // Pause or play gifs
     function animateGifs() {
         var state = $(this).data("state");
         if (state === "still") {
@@ -57,42 +51,38 @@ $(document).ready(function() {
         }
     }
 
-
+    // Generates buttons based on user searches or topics
     function createButtons() {
         $("#topic-buttons").empty();
 
         for (var i = 0; i < topics.length; i++) {
-            var a = $("<button>"); //Create Button
-            a.addClass("topic"); //Adds class to button
-            a.attr("data-name", topics[i]); //Adds a data value to button
-            a.text(topics[i]); //Adds text to button
+            var a = $("<button class='btn btn-primary'>");
+            a.addClass("topic"); 
+            a.attr("data-name", topics[i]); 
+            a.text(topics[i]); 
             $("#topic-buttons").append(a);
-
-            // **TEST**
-            // console.log(a);
         }
 
     }
 
-
-
+    // Click the search button to create a topic button based on user search
     $("#add-topic").on("click", function(event) {
         event.preventDefault(); //Prevents default functionality of button
-        var topic = $("#topic-input").val().trim(); //Extact value from text
+        var topic = $("#topic-input").val().trim();
         topics.push(topic);
         createButtons();
 
-        // **TEST**
-        // console.log(topics, "topics");
-
-
     });
 
+    // Generates pre-set topics automatically
+    createButtons();
 
-
+    // Click a button to populate page
     $(document).on("click", ".topic", displayGifs);
 
+    // Click on gif to pause or play
     $(document).on("click", ".animation", animateGifs);
+
 
 });
 
